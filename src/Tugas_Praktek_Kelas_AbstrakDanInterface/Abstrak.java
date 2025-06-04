96d0dab57c9cd3b7eea9ca740217ed5a5cbc6282
Rianda Rafqi <riandarafqi321@gmail.com>
2025-06-05 03:31:33 +0700

    Mengerjakan tugas pertemuan 11

--- src/Tugas_Praktek_Kelas_AbstrakDanInterface/Abstrak.java
+++
@@ -1,5 +0,0 @@
-package Tugas_Praktek_Kelas_AbstrakDanInterface;
-
-public class Abstrak {
-
-}
---
+++ src/Tugas_Praktek_Kelas_AbstrakDanInterface/Peminjaman.java
@@ -0,0 +1,8 @@
+package Tugas_Praktek_Kelas_AbstrakDanInterface;
+
+
+//Interface untuk peminjaman buku
+public interface Peminjaman {
+void pinjam();  // Metode untuk meminjam buku
+void kembalikan();  // Metode untuk mengembalikan buku
+}
